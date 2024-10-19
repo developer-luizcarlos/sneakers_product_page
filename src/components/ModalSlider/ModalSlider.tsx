e7c3335780c9bd4c78b92a942bac7e126d16bc51
Luiz Carlos <developer.luizcarlos@outlook.com>
2024-10-18 21:00:11 -0300

    fix: modal slider select images icon source changed

--- src/components/ModalSlider/ModalSlider.tsx
+++ src/components/ModalSlider/ModalSlider.tsx
@@ -4,6 +4,7 @@
 import { MouseEventHandler,useState } from "react";
 import { IoClose } from "react-icons/io5";
 import { images } from "@/app/(gallery)/gallery";
+import { thumbnails } from "@/app/(thumbnail)/thumbnail";
 
 type Props = {
   visibility: boolean;
@@ -35,38 +36,40 @@ export default function ModalSlider({ visibility,eventHandler }: Props) {
 
   return (
     <div
-      className={!visibility ? "hidden" : "w-dvw h-dvh absolute top-0 left-0 bg-slate-950 bg-opacity-65 flex items-center justify-center z-40"}>
-      <div className="w-[430px] flex items-center flex-col gap-y-4">
+      className={!visibility ? "hidden" : "w-dvw h-dvh absolute top-0 bottom-0 right-0 left-0 bg-slate-950 bg-opacity-65 flex items-center justify-center z-40"}>
+      <div
+        className="w-[400px] flex items-center flex-col gap-y-4">
         <div className="w-full flex items-center justify-end">
           <button
             onClick={eventHandler}
             className="cursor-pointer">
-            <IoClose className="w-12 h-7 hover:text-paleOrange" />
+            <IoClose className="w-12 h-7 hover:text-paleOrange duration-500" />
           </button>
         </div>
-        <div className="w-full flex  items-center justify-center">
-          <button
-            onClick={() => { decrementImageIndex(); }}
-            className="relative top-1/2 left-6 bg-grayishBlue w-12 h-12 rounded-[50%] p-5 flex items-center justify-center cursor-pointer duration-300 hover:text-paleOrange">
-            <span>&lt;</span>
-          </button>
-          <img
-            src={images[imageIndex].src}
-            alt="product photo"
-            className="w-full object-cover rounded-xl"
-          />
-          <button
-            onClick={() => { incrementImageIndex(); }}
-            className="relative top-1/2 right-6 bg-grayishBlue w-12 h-12 rounded-[50%] p-5 flex items-center justify-center cursor-pointer duration-300 hover:text-paleOrange">
-            &gt;
-          </button>
+        <div className="w-full flex items-center justify-center">
+          <div className="w-full relative h-[400px]">
+            <div
+              className="absolute top-0 left-0 bottom-0 right-0 h-full w-full flex items-center justify-between">
+              <button
+                onClick={() => decrementImageIndex()}
+                className="w-14 h-14 bg-zinc-200 flex items-center justify-center font-bold text-xl rounded-full relative -left-8 hover:text-paleOrange active:text-paleOrange duration-500">
+                &lt;
+              </button>
+              <button
+                onClick={() => incrementImageIndex()}
+                className="w-14 h-14 bg-zinc-200 flex items-center justify-center font-bold text-xl rounded-full relative -right-8 hover:text-paleOrange active:text-paleOrange duration-500">
+                &gt;
+              </button>
+            </div>
+            <img src={images[imageIndex].src} alt="product photo" className="w-full h-full rounded-md" />
+          </div>
         </div>
         <div className="w-full flex items-center justify-center gap-3">
-          {images.map((image,index) => {
+          {thumbnails.map((image,index) => {
             return <div key={image.src} className={imageIndex === index ? "border-2 border-paleOrange rounded-lg" : ""}>
               <img
                 src={image.src}
-                alt="product photo" className={index === imageIndex ? "object-cover w-24 h-24 grayscale rounded-lg cursor-pointer" : "object-cover w-24 h-24 rounded-lg cursor-pointer"}
+                alt="product photo" className={index === imageIndex ? "object-cover w-24 h-24 brightness-75 rounded-lg cursor-pointer" : "object-cover w-24 h-24 rounded-lg cursor-pointer"}
                 onClick={() => { setImageIndex(index); }}
               />
             </div>;
